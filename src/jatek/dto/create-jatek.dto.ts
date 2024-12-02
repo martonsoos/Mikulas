1a33f80c6e1ca2336ef7c788e50fe543114ad69e
Marton <martonsoos10@gmail.com>
2024-12-02 13:45:09 +0100

    jatek dto

--- src/jatek/dto/create-jatek.dto.ts
+++ src/jatek/dto/create-jatek.dto.ts
@@ -1,1 +1,14 @@
-export class CreateJatekDto {}
+import { IsIn, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateJatekDto {
+  @IsString()
+  @IsNotEmpty()
+  nev: string;
+
+  @IsString()
+  @IsIn(['fa', 'fém', 'műanyag', 'egyéb'])
+  anyag: string;
+
+  @IsNumber()
+  suly: number;
+}
